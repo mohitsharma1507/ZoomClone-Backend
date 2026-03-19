3aa7b7b2e568eeb62cf1d428c06b11d13c14ded2
MOHIT <mohitjyouti2003@gmail.com>
2026-03-19 19:42:14 +0530

    add files

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://zoom-clone-frontend-jamt.vercel.app/",
+      "http://zoom-clone-frontend-jamt.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
