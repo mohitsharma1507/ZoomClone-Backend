34aa3ea4eca06911ede482c660308ca889f41c7d
MOHIT <mohitjyouti2003@gmail.com>
2026-03-19 18:55:49 +0530

    add files

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const io = connectToSocket(server);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", `${process.env.FRONTEND_URL}`],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
