cd36a45f3f568d140d942620adaaf0eb9e91e48f
MOHIT <mohitjyouti2003@gmail.com>
2026-03-20 20:10:19 +0530

    add files

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 
 const transporter = nodeMailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
+  port: Number(process.env.SMTP_PORT),
   secure: false,
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
